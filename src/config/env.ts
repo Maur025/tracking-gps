cbe8e790db1c220972b2abf4e963aee6b1b5328b
Maur025 <mmoyapalli@gmail.com>
2025-04-11 09:50:39 -0400

    fix: update HOST type to string | undefined and adjust setHost method

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,7 +3,7 @@ import Environment from '@models/interface/environment.interface';
 const { STATIC_PATH, HOST, PORT, BACKEND_URL, TRACK_URL } = process.env;
 
 const environment: Environment = {
-	HOST: HOST ?? '172.20.50.60',
+	HOST: HOST,
 	PORT: Number(PORT ?? 7767),
 	STATIC_PATH: STATIC_PATH ?? 'public',
 
