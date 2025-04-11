3d914b0dbcbfc91dd5d0701156fb3c894bc9e6e1
maur025 <mmoyapalli@gmail.com>
2025-04-10 23:39:08 -0400

    feat: add routing structure and environment configuration

--- src/models/interface/server-builder.interface.ts
+++ src/models/interface/server-builder.interface.ts
@@ -1,9 +1,10 @@
 import { Server } from 'http';
 import ServerBuilderResponse from './server-builder-response.interface';
+import { Router } from 'express';
 
 export default interface IServerBuilder {
 	applyMiddlewares(): this;
-	applyRoutes(): this;
+	applyRoutes(router: Router): this;
 	configureStatic(): this;
 	build(): ServerBuilderResponse;
 	start(): Server;
