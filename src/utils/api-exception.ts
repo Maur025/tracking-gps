49e49a5b70c0ad0a19f2e6d0539a1dde91638d06
Maur025 <mmoyapalli@gmail.com>
2025-04-15 10:59:57 -0400

    feat: update TypeScript configuration and improve error handling in API client, to build project

--- src/utils/api-exception.ts
+++ src/utils/api-exception.ts
@@ -3,7 +3,7 @@ export class ApiException extends Error {
 	public message: string;
 	public error: boolean;
 
-	constructor(status: number, message: string, error = true) {
+	constructor(status: number, message: string, error: boolean = true) {
 		super(message);
 		this.status = status;
 		this.message = message;
