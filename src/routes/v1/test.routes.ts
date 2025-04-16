01c27a188ae350ac2fa4c0071138e3b7c6881bce
Maur025 <mmoyapalli@gmail.com>
2025-04-16 10:28:18 -0400

    feat: integrate tsyringe for dependency injection in DeviceCache, TestCmd, TrackService, and TestController refactor: update socket-track-client to log device payload fix: change DeviceResponse to extend DataResponse

--- src/routes/v1/test.routes.ts
+++ src/routes/v1/test.routes.ts
@@ -1,8 +1,9 @@
 import TestController from '@controllers/test.controller';
 import { Router } from 'express';
+import { container } from 'tsyringe';
 
 const router = Router();
-const testController = new TestController();
+const testController = container.resolve(TestController);
 
 router.get('/', testController.getTest);
 router.get('/two', testController.getTestTwo);
