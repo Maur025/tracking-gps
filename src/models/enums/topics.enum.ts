6771c1e8e86d3d0f52514674aa87ba7cd3393617
Maur025 <mmoyapalli@gmail.com>
2025-04-22 17:20:52 -0400

    feat: update Topics enum and refactor socket event handling for device management

--- src/models/enums/topics.enum.ts
+++ src/models/enums/topics.enum.ts
@@ -7,7 +7,10 @@ export enum Topics {
 	MESSAGE = 'message',
 	DEVICES = 'devices',
 	DEVICE_NEW = 'device.new',
+	DEVICE_REMOVE = 'device.remove',
 	DEVICE_TRACK = 'device.track',
+	DEVICE_LAST = 'device.last',
+	DEVICE_UNSUBSCRIBE = 'device.unsubscribe',
 	DEVICE_UNSUBSCRIBE_ALL = 'device.unsubscribe.all',
 	DEVICE_SUBSCRIBE = 'device.subscribe',
 }
--- src/socket/server/socket-reply.ts
+++ src/socket/server/socket-reply.ts
@@ -2,25 +2,44 @@ import { Topics } from '@models/enums/topics.enum';
 import { connect } from '@socket/client/socket-track-reply-client';
 import { Socket } from 'socket.io';
 import { Socket as SocketClient } from 'socket.io-client';
+import Device from '../../models/entity/device';
 
 const clientReply: SocketClient = connect();
 
+const {
+	MESSAGE,
+	DEVICES,
+	DEVICE_NEW,
+	DEVICE_REMOVE,
+	DEVICE_TRACK,
+	DEVICE_LAST,
+	DEVICE_SUBSCRIBE,
+	DEVICE_UNSUBSCRIBE,
+	DEVICE_UNSUBSCRIBE_ALL,
+} = Topics;
+
 export const socketReply = (socket: Socket) => {
-	clientReply.on(Topics.MESSAGE, payload => {
-		console.log(payload);
+	clientReply.on(MESSAGE, payload => socket.emit(MESSAGE, payload));
 
-		socket.emit(Topics.MESSAGE, payload);
-	});
+	clientReply.on(DEVICES, (payload: Device[]) => socket.emit(DEVICES, payload));
 
-	clientReply.on(Topics.DEVICES, payload => {
-		socket.emit(Topics.MESSAGE, payload);
-	});
+	clientReply.on(DEVICE_NEW, payload => socket.emit(DEVICE_NEW, payload));
 
-	clientReply.on(Topics.DEVICE_NEW, payload => {
-		socket.emit(Topics.DEVICE_NEW, payload);
-	});
+	clientReply.on(DEVICE_REMOVE, payload => socket.emit(DEVICE_REMOVE, payload));
 
-	clientReply.on(Topics.DEVICE_TRACK, payload => {
-		socket.emit(Topics.DEVICE_TRACK, payload);
-	});
+	clientReply.on(DEVICE_TRACK, payload => socket.emit(DEVICE_TRACK, payload));
+
+	clientReply.on(DEVICE_LAST, payload => socket.emit(DEVICE_LAST, payload));
+
+	clientReply.on(DEVICE_SUBSCRIBE, payload =>
+		socket.emit(DEVICE_SUBSCRIBE, payload)
+	);
+
+	clientReply.on(DEVICE_UNSUBSCRIBE, payload =>
+		socket.emit(DEVICE_UNSUBSCRIBE, payload)
+	);
+
+	clientReply.on(DEVICE_UNSUBSCRIBE_ALL, payload =>
+		socket.emit(DEVICE_UNSUBSCRIBE_ALL, payload)
+	);
 };
