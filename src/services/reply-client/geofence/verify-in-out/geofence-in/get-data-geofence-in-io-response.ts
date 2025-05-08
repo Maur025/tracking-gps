fda7249d55015b9bc621200a1219eda340610839
Maur025 <mmoyapalli@gmail.com>
2025-05-08 17:23:22 -0400

    Refactor geofence response handling to use shared pipe utility
    - Updated imports in geofence-in and geofence-out response files to use the new pipe utility from '@maur025/core-common'.
    - Removed the local pipe utility implementation in pipe-util.ts as it is no longer needed.

--- src/services/reply-client/geofence/verify-in-out/geofence-in/get-data-geofence-in-io-response.ts
+++ src/services/reply-client/geofence/verify-in-out/geofence-in/get-data-geofence-in-io-response.ts
@@ -1,8 +1,8 @@
 import GeofenceInIoResponse from '@models/dto/response/socket/geofence-in-io-response';
 import GeofenceIoResponse from '@models/dto/response/socket/geofence-io-response';
 import GeofenceIn from '@models/entity/geofence-in';
-import { pipe } from '@utils/pipe-util';
 import { getGeofenceInIoResponse } from '../get-geofence-in-io-response';
+import { pipe } from '@maur025/core-common';
 
 interface Request {
 	deviceId: string;
