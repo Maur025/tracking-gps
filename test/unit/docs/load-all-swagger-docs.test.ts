af6986dab39a24794457b9502b71547a968dee05
Maur025 <mmoyapalli@gmail.com>
2025-06-23 15:02:39 -0400

    feat: add unit tests for Swagger configuration and loading all Swagger docs

---
+++ test/unit/docs/load-all-swagger-docs.test.ts
@@ -0,0 +1,44 @@
+import { beforeAll, beforeEach, describe, expect, test, vi } from 'vitest';
+
+vi.mock('@routes/v1/test/test.swagger', () => ({
+	testSwagger: vi.fn(),
+}));
+
+import { testSwagger } from '@routes/v1/test/test.swagger';
+import { loadAllSwaggerDocs } from '@src/docs/load-all-swagger-docs';
+import ZodSwaggerGenerator from '@src/docs/swagger/zod-swagger-generator';
+import { container } from 'tsyringe';
+
+describe('load all swagger docs test', () => {
+	const BASE_PATH: string = '/api/v1';
+	let mockZodSwaggerGenerator: ZodSwaggerGenerator;
+	const mockTags = vi.fn();
+
+	beforeAll(() => {
+		const MockZodSwaggerGenerator = vi.fn();
+		MockZodSwaggerGenerator.prototype.setTags = mockTags;
+
+		mockZodSwaggerGenerator = new MockZodSwaggerGenerator();
+	});
+
+	beforeEach(() => {
+		vi.resetAllMocks();
+
+		container.registerInstance(ZodSwaggerGenerator, mockZodSwaggerGenerator);
+	});
+
+	test('should load all docs data', () => {
+		loadAllSwaggerDocs();
+
+		expect(testSwagger).toHaveBeenCalledWith(
+			expect.objectContaining({ path: `${BASE_PATH}/test`, tag: 'TEST' }),
+		);
+
+		expect(mockTags).toHaveBeenCalledWith([
+			expect.objectContaining({
+				name: 'TEST',
+				description: expect.any(String),
+			}),
+		]);
+	});
+});
