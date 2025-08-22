b0bed3124918c8a27c2cd6084e6326b0a0bb07b3
Maur025 <mmoyapalli@gmail.com>
2025-08-22 18:07:54 -0400

    Changes made to control and start the WhatsApp service correctly, channel initialization

--- src/app/notification/service/whatsapp/send-notification-to-whatsapp.ts
+++ src/app/notification/service/whatsapp/send-notification-to-whatsapp.ts
@@ -1,27 +1,64 @@
-import { Browser, chromium } from 'playwright';
-
+import { loggerDebug } from '@maur025/core-logger';
+import path from 'node:path';
+import { chromium } from 'playwright';
+// ONLY DEMO... REMOVE LATER
 export const sendNotificationToWhatsapp = async (): Promise<void> => {
-	let browser: Browser | null = null;
-	try {
-		browser = await chromium.launch({
+	const browser = await chromium.launchPersistentContext(
+		`${path.resolve(process.cwd(), 'tmp/playwright-profile')}`,
+		{
+			headless: false,
+			executablePath: '/usr/bin/google-chrome-stable',
+		},
+	);
+
+	const page = await browser.newPage();
+	await page.goto('https://web.whatsapp.com');
+
+	await page.screenshot({ path: 'debug.png' });
+
+	const searchChatTextBox = await page.waitForSelector(
+		'[aria-label="Search input textbox"]',
+	);
+
+	if (searchChatTextBox) {
+		console.log(
+			'existe el textbox asi que puedo cerrar el navegador e iniciarlo sin la opcion de visualizacion',
+		);
+	}
+
+	await page.close();
+	await browser.close();
+
+	const browserHeadless = await chromium.launchPersistentContext(
+		`${path.resolve(process.cwd(), 'tmp/playwright-profile')}`,
+		{
 			headless: true,
 			executablePath: '/usr/bin/google-chrome-stable',
-		});
+		},
+	);
 
-		const context = await browser.newContext({
-			viewport: { width: 1920, height: 1080 },
-		});
-		const page = await context.newPage();
+	const message =
+		'TEST OF RULE WITH GOEFENCE IN-OUT\nDispositivo: 3165cdc688df6\nlat: -16.529648\nlon: -68.070368\nubicacion:https://maps.app.goo.gl/2Yk64tfZmsepXJh59';
 
-		await page.goto('https://web.whatsapp.com');
+	const textoUrl = encodeURIComponent(message);
 
-		await page.screenshot({ path: 'debug.png' });
+	console.log(textoUrl);
 
-		await page.close();
-		await context.close();
-	} finally {
-		if (browser) {
-			await browser.close();
-		}
-	}
+	const pageToSend = await browserHeadless.newPage();
+	await pageToSend.goto(
+		`https://web.whatsapp.com/send?phone=59160174745&text=${textoUrl}`,
+	);
+
+	const buttonSend = await pageToSend.waitForSelector('[aria-label="Send"]', {
+		state: 'attached',
+	});
+
+	await pageToSend.screenshot({ path: 'button-send.png' });
+
+	await buttonSend.click();
+
+	await pageToSend.waitForTimeout(1000);
+	await pageToSend.close();
+
+	loggerDebug(`[WHATSAPP] (func) notification sent to number`);
 };
