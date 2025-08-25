c201e1bdfdc634450e65291bf0ff585417ba46f7
Maur025 <mmoyapalli@gmail.com>
2025-08-25 13:14:23 -0400

    Add DeviceNotificationSchema and notificationBuildByAlertList function

--- src/app/device/entity/device-rule-alert-to-launch.ts
+++ src/app/device/entity/device-rule-alert-to-launch.ts
@@ -1,17 +1,22 @@
-import z, { boolean, object, string } from 'zod/v4';
+import z, { boolean, number, object, string } from 'zod/v4';
 import { DeviceRuleAlertToLaunchType } from './device-rule-alert-to-launch-type';
 
 export const DeviceRuleAlertToLaunch = object({
 	ruleGeofenceRegistryId: string().nonempty().optional(),
 	ruleRegistryStatesId: string().nonempty().optional(),
-	alertType: DeviceRuleAlertToLaunchType,
 	alertId: string().nullable(),
 	ruleId: string().nonempty(),
+	deviceId: string().nonempty().optional(),
+	geofenceId: string().nonempty().optional(),
+	alertType: DeviceRuleAlertToLaunchType,
 	isGeofenceIn: boolean().optional(),
 	speed: string().optional(),
 	fuel: string().optional(),
 	battery: string().optional(),
 	ignition: string().optional(),
+	timestamp: number().nonnegative().optional(),
+	lat: number().min(-90).max(90).optional(),
+	lon: number().min(-180).max(180).optional(),
 });
 
 export type DeviceRuleAlertToLaunch = z.infer<typeof DeviceRuleAlertToLaunch>;
